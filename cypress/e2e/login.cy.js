38c74acfb6c0f26af81a7ff8551d8b7d6eb717ca
RafaelFelice <rafaelfelice92@gmail.com>
2022-11-12 17:21:02 -0300

    criando describe Unsuccessfully login e teste Login with an invalid email

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -10,5 +10,16 @@ describe('Successfully login', () => {
       cy.get('[data-testid="entrar"]').click()
       cy.get('h1').should('contain', "Bem Vindo  ")
     })
+})
 
+describe('Unsuccessfully login', () => {
+    before(()=>{
+        cy.visit('https://front.serverest.dev/login')
+      })
+    it('Login with an invalid email', () => {
+      cy.get('#email').type('invalid@qa.com')
+      cy.get('#password').type('teste')
+      cy.get('[data-testid="entrar"]').click()
+      cy.get('.alert > :nth-child(2)').should('have.text', "Email e/ou senha inválidos")
+    })
 })
