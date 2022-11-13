4b41a78f04fd1acb6732fb6a6b05a2a28bd1e416
RafaelFelice <rafaelfelice92@gmail.com>
2022-11-13 14:06:53 -0300

    criando cenários de testes Unsuccessfully login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -13,7 +13,7 @@ describe('Successfully login', () => {
 })
 
 describe('Unsuccessfully login', () => {
-    before(()=>{
+    beforeEach(()=>{
         cy.visit('https://front.serverest.dev/login')
       })
     it('Login with an invalid email', () => {
@@ -22,4 +22,41 @@ describe('Unsuccessfully login', () => {
       cy.get('[data-testid="entrar"]').click()
       cy.get('.alert > :nth-child(2)').should('have.text', "Email e/ou senha inválidos")
     })
+
+    it('Login with an invalid password', () => {
+      cy.get('#email').type('fulano@qa.com')
+      cy.get('#password').type('invalid')
+      cy.get('[data-testid="entrar"]').click()
+      cy.get('.alert > :nth-child(2)').should('have.text', "Email e/ou senha inválidos")
+    })
+
+    it('Login with invalid email and password', () => {
+      cy.get('#email').type('invalid@qa.com')
+      cy.get('#password').type('invalid')
+      cy.get('[data-testid="entrar"]').click()
+      cy.get('.alert > :nth-child(2)').should('have.text', "Email e/ou senha inválidos")
+    })
+
+    it('Login with just the blank email', () => {
+      cy.get('#email').type('{BACKSPACE}')
+      cy.get('#password').type('teste')
+      cy.get('[data-testid="entrar"]').click()
+      cy.get('.alert > :nth-child(2)').should('have.text', "Email é obrigatório")
+    })
+
+    it('Login with just the blank password', () => {
+      cy.get('#email').type('fulano@qa.com')
+      cy.get('#password').type('{BACKSPACE}')
+      cy.get('[data-testid="entrar"]').click()
+      cy.get('.alert > :nth-child(2)').should('have.text', "Password é obrigatório")
+    })
+
+    it('Login with the blank email and password', () => {
+      cy.get('#email').type('{BACKSPACE}')
+      cy.get('#password').type('{BACKSPACE}')
+      cy.get('[data-testid="entrar"]').click()
+      cy.get(':nth-child(3) > :nth-child(2)').should('have.text', "Email é obrigatório")
+      cy.get(':nth-child(4) > :nth-child(2)').should('have.text', "Password é obrigatório")
+    })
+
 })
